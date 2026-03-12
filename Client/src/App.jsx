c66b74ef48514539fdbc3fa07b600a64be683b5a
Anuradha <anuradhal4264@gmail.com>
2026-03-12 20:10:18 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import Sidebar from './Components/Sidebar'
+import Chatbox from './Components/Chatbox'
+import Credits from './Pages/Credits'
+import Community from './Pages/Community'
+import Loading from './Pages/Loading'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { assets } from './assets/assets'
+import './assets/prism.css'
+import { useAppContext } from './Context/AppContext'
+import Login from './Pages/Login'
+
+const App = () => {
+
+  const {user} = useAppContext()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const {pathname} = useLocation()
+
+  if(pathname === '/loading') return <Loading />
+
+  return (
+    <>
+    {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' 
+    onClick={()=>setIsMenuOpen(true)}/>}
+
+    {user ? (
+       <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+    <div className='flex h-screen w-screen'>
+      <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+      <Routes>
+        <Route path= "/" element={<Chatbox />} />
+        <Route path= "/credits" element={<Credits />} />
+        <Route path= "/community" element={<Community />} />
+      </Routes>
+    </div>
+    </div>
+    ) :(
+      <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+        <Login />
+      </div>
+    )}
+    </>
+  )
+}
+
+export default App
