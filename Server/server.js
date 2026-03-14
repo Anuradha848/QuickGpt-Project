d32ec03607a28e83044aa2e6f842b73bb895137c
Anuradha <anuradhal4264@gmail.com>
2026-03-14 19:34:04 +0530

    test server

--- Server/server.js
+++ Server/server.js
@@ -1,43 +1,29 @@
-// import express from "express";
-// import "dotenv/config";
-// import cors from "cors";
-// import connectDB from "./configs/db.js";
-// import userRouter from "./routes/userRoutes.js";
-// import chatRouter from "./routes/chatRoutes.js";
-// import messageRouter from "./routes/messageRoutes.js";
-// import creditRouter from "./routes/creditRoutes.js";
-// import { stripeWebhooks } from "./controllers/webhooks.js";
-
-// const app = express();
-
-// // middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // connect DB when a request happens
-// connectDB();
-
-// // routes
-// app.get("/", (req, res) => res.send("server is Live!"));
-// app.use("/api/user", userRouter);
-// app.use("/api/chat", chatRouter);
-// app.use("/api/message", messageRouter);
-// app.use("/api/credit", creditRouter);
-
-// app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
-
-// export default app;
-
 import express from "express";
+import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import chatRouter from "./routes/chatRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import creditRouter from "./routes/creditRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("server is Live!");
-});
+// connect DB when a request happens
+connectDB();
+
+// routes
+app.get("/", (req, res) => res.send("server is Live!"));
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+app.use("/api/credit", creditRouter);
+
+app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 export default app;
